f4d8cfa606357e629beb11cd339ee0363a1fc8b8
Oliver <olichase91@gmail.com>
2023-04-25 14:41:25 -0700

    Scraping is Awesome!

--- lib/scraper.py
+++ lib/scraper.py
@@ -3,4 +3,22 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+html = requests.get("https://www.npr.org/2023/04/25/1171924186/contact-is-lost-with-a-japanese-spacecraft-attempting-to-land-on-the-moon", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+article_text = (doc.select("p"))
+
+# for p in article_text:
+#     print(p.text)
+
+
+
+name = doc.select(".storytitle")[0].name
+
+print(name)
+
+
+attrs = doc.select(".storytitle")[0].attrs
+
+print(attrs)
